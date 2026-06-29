dd2f6a142c795f78ca7223c20d9d64c4937daa0b
teammilanofficial <teammilanofficial.support@gmail.com>
2026-06-29 18:51:24 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,14 +1,14 @@
 window.RANKGRID_SUPABASE = null;
 
 try {
-  const RANKGRID_SUPABASE_URL = "https://nmlxcodtilehtdremrsg.supabase.co/rest/v1/";
-  const RANKGRID_SUPABASE_ANON_KEY = "https://nmlxcodtilehtdremrsg.supabase.co/rest/v1/";
+  const RANKGRID_SUPABASE_URL = "https://nmlxcodtilehtdremrsg.supabase.co";
+
+  const RANKGRID_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tbHhjb2R0aWxlaHRkcmVtcnNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzA0MjAsImV4cCI6MjA5ODMwNjQyMH0.9sWVFsrs6utQTN1tPHTJxe_N2UEkcAtXEhpirkXt9AY";
 
   if (
     window.supabase &&
     RANKGRID_SUPABASE_URL &&
     RANKGRID_SUPABASE_ANON_KEY &&
-    !RANKGRID_SUPABASE_URL.includes("PASTE_") &&
     !RANKGRID_SUPABASE_ANON_KEY.includes("PASTE_")
   ) {
     window.RANKGRID_SUPABASE = window.supabase.createClient(
@@ -16,7 +16,7 @@ try {
       RANKGRID_SUPABASE_ANON_KEY
     );
   } else {
-    console.log("Supabase is not configured yet.");
+    console.log("Supabase not configured correctly.");
   }
 } catch (error) {
   console.log("Supabase config error:", error);
