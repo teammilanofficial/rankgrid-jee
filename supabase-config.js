c630e2d0c1bcb3fa8a494c13ffc900bbf6a95e34
teammilanofficial <teammilanofficial.support@gmail.com>
2026-06-29 17:42:58 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://nmlxcodtilehtdremrsg.supabase.co/rest/v1/";
+
+const SUPABASE_ANON_KEY = "https://nmlxcodtilehtdremrsg.supabase.co/rest/v1/";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
